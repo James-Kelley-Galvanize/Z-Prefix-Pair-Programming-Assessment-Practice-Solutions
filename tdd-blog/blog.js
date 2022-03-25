8a79152b7ba2d0148c455246ddebc545053895bc
James-Kelley-Galvanize <james.kelley@galvanize.com>
2022-03-25 10:21:06 -0600

    solve blog and calculator

--- tdd-blog/blog.js
+++ tdd-blog/blog.js
@@ -1,9 +1,61 @@
 class Blog {
   constructor() {
-
+    this.posts = [];
+    this.comments = {};
   }
-
-
+  getBlogPosts() {
+    return this.posts.length
+      ? this.posts.map((post) => post.title)
+      : `There are 0 blog posts`;
+  }
+  addBlogPost(title, author, genre) {
+    this.comments[title] = { comments: [] };
+    this.posts.push({ title, author, genre });
+  }
+  getBlogAuthors() {
+    return this.posts.map((post) => post.author);
+  }
+  getSpecificBlogAuthor(searchedTitle) {
+    let correctEntry = this.posts.find((post) => post.title === searchedTitle);
+    return correctEntry
+      ? correctEntry.author
+        ? correctEntry.author
+        : `Blog has no author`
+      : `No such blog post`;
+  }
+  getBlogsFromGenre(searchedGenre) {
+    return this.posts.filter((post) => post.genre === searchedGenre);
+  }
+  getAllGenres() {
+    let genres = this.posts.map((post) => post.genre);
+    return genres.length ? genres : `There are no genres available`;
+  }
+  addComment(postTitle, user, comment) {
+    this.comments[postTitle].comments.push({ user, comment });
+  }
+  getBlogComments(postTitle) {
+    return this.comments[postTitle].comments.length
+      ? [this.comments[postTitle]]
+      : [];
+  }
+  getCommentsBy(user) {
+    let out = {
+      user,
+      comments: [],
+    };
+    Object.keys(this.comments).forEach((title) => {
+      let comments = this.comments[title].comments;
+      comments.forEach((comment) => {
+        if (comment.user === user)
+          out.comments.push({ article: title, comment: comment.comment });
+      });
+    });
+    return out;
+  }
+  deleteBlog(titleToDelete) {
+    this.posts = this.posts.filter((post) => post.title !== titleToDelete);
+    delete this.comments[titleToDelete];
+  }
 }
 
-module.exports = Blog
+module.exports = Blog;
--- tdd-calculator/calculator.js
+++ tdd-calculator/calculator.js
@@ -1,7 +1,47 @@
 class Calculator {
   constructor() {
-    
+    this.state = 0;
+  }
+  setState(val) {
+    this.state = val;
+    return this.state;
+  }
+  getCurrentState() {
+    return this.state;
+  }
+  addInputs(...numbers) {
+    let output = numbers.reduce((acc, val) => acc + val, 0);
+    return this.setState(output);
+  }
+  multiplyInputs(...numbers) {
+    let output = numbers.reduce((acc, val) => acc * val, 1);
+    return this.setState(output);
+  }
+  divideInputs(a, b) {
+    let output = b ? a / b : a;
+    return this.setState(output);
+  }
+  subtractInputs(...args) {
+    let output = args.reduce((acc, val) => acc - val);
+    return this.setState(output);
+  }
+  toPower(a, b) {
+    let output = Math.pow(a, b);
+    return this.setState(output);
+  }
+  squareRoot(a) {
+    let root = Math.pow(a, 0.5);
+    let output = root % 1 !== 0 ? parseFloat(root.toFixed(2)) : root;
+    return this.setState(output);
+  }
+  clearCurrentStatus() {
+    this.setState(0);
+
+    return true;
+  }
+  negPosReversal(a) {
+    return 0 - a;
   }
 }
 
-module.exports = Calculator
+module.exports = Calculator;
