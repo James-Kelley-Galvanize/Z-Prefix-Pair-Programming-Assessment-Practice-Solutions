ef5a6d5d3ac0320ef2cfb6c0a8ba9bb6090447cc
James-Kelley-Galvanize <james.kelley@galvanize.com>
2022-03-25 10:41:05 -0600

    finish set

--- tdd-set-javascript/set.js
+++ tdd-set-javascript/set.js
@@ -1,7 +1,27 @@
 class GalvanizeSet {
   constructor() {
-
+    this.values = {};
+  }
+  get allValues() {
+    return Object.keys(this.values);
+  }
+  insert(item) {
+    // console.log(`adding `, item, `to set`);
+    this.values[item] = true;
+  }
+  getSize() {
+    return this.allValues.length;
+  }
+  contains(value) {
+    return value in this.values;
+  }
+  delete(value) {
+    if (value in this.values) {
+      delete this.values[value];
+      return true;
+    }
+    return false;
   }
 }
 
-module.exports = GalvanizeSet
+module.exports = GalvanizeSet;
